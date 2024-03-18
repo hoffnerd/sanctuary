ec43822985f5c8f0454395489a06f14de7aa3838
Dylan Hoffner <hoffnerd@msu.edu>
2024-03-17 23:35:12 -0400

    added a way to make change logs with markdown

---
+++ src/actions/markdown.js
@@ -0,0 +1,71 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import { unified } from "unified";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const markdownDirectory = path.join(process.cwd(), "src", "_markdown");
+
+const getMarkdownFiles = (endPath="") => {
+    const fullPath = path.join(markdownDirectory, endPath);
+    return fs.readdirSync(fullPath);
+}
+
+const getParser = () => (
+    unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(remarkGfm)
+    .use(rehypePrettyCode, { theme: "one-dark-pro" })
+    .use(rehypeStringify)
+    .use(rehypeStringify)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, {
+        content: (arg) => ({
+            type: "element",
+            tagName: "a",
+            properties: {
+                href: `#${String(arg.properties?.id)}`,
+                style: "margin-right: 10px",
+            },
+            children: [{ type: "text", value: "#" }],
+        }),
+    })
+)
+
+// small speedup from caching this parser
+const parser = getParser();
+
+export const readMarkdownFileById = async (endPath="", id) => {
+    const realId = id.replace(/\.md$/, "");
+    const fullPath = path.join(markdownDirectory, endPath, `${realId}.md`);
+    const { data, content } = matter( await fs.promises.readFile(fullPath, "utf8") );
+
+    const html = await parser.process(content);
+    const date = data.date;
+
+    return {
+        ...data,
+        title: data.title,
+        id: realId,
+        date: `${date.toISOString().slice(0, 10)}`,
+        html: html.value.toString(),
+    };
+}
+
+export const readPageMarkdown = async (string_) => {
+    const { data, content } = matter( fs.readFileSync(path.join("_pages", string_), "utf8") );
+    const html = await parser.process(content);
+    return { ...data, html: html.value.toString() }
+}
+
+export const readAllMarkdownFiles = async (endPath="") => {
+    const markdownFiles = await Promise.all(getMarkdownFiles(endPath).map((id) => readMarkdownFileById(endPath, id)));
+    return markdownFiles.sort((file1, file2) => (file1.date > file2.date ? -1 : 1));
+}
---
+++ src/app/change-log/page.js
@@ -0,0 +1,65 @@
+// React/Next -----------------------------------------------------------------------
+// Styles ---------------------------------------------------------------------------
+import styles from "@/styles/components/SaveFile.module.css";
+// Components -----------------------------------------------------------------------
+import { readAllMarkdownFiles } from '@/actions/markdown';
+import Link from 'next/link';
+import { format } from "date-fns";
+// Data -----------------------------------------------------------------------------
+// Other ----------------------------------------------------------------------------
+
+
+
+//______________________________________________________________________________________
+// ===== Page Meta Data =====
+export const metadata = {
+    title: `Change Log | Sanctuary`,
+    description: 'All the changes that have been made to the site.',
+}
+
+//______________________________________________________________________________________
+// ===== Component =====
+export default async function Page() {
+
+    //______________________________________________________________________________________
+    // ===== Server Actions =====
+    const logs = await readAllMarkdownFiles("change-log");
+
+    /*
+        = New Content =
+        ```
+        -
+        ```
+
+        
+        = Content Changed =
+        ```
+        -
+        ```
+
+        
+        = Bug Fixes =
+        ```
+        -
+        ```
+    */
+
+    //______________________________________________________________________________________
+    // ===== Component Return =====
+    return (
+        <div className="container">
+            <h1 className="text-5xl font-black pb-10">Change Logs</h1>
+            {logs.map(({ id, date, title, html, }) => (
+                <div key={id} className="py-5">
+                    <div className={`${styles.saveFile} neonBorder neonBoxShadowGlow purple`}>
+                        <div className="p-10">
+                            <h2 className="text-2xl font-black">{title}</h2>
+                            <h3 className="text-lg font-black pb-5">{format(date, "MMMM do, yyyy")}</h3>
+                            <div dangerouslySetInnerHTML={{ __html: html }} />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
